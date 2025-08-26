8c01519374ea8747bc9f008c0736e37764870327
laijj-GeneditBio <Jiajia.Lai@geneditbio.com>
2025-08-26 16:21:14 +0800

    commit3 add pytest.py

--- pytest.py
+++ pytest.py
@@ -10,11 +10,20 @@ print('yyyyyyyyyyyyyyyyyyyyyyyyyy')
 print('yyyyyyyyyyyyyyyyyyyyyyyyyy')
 print('yyyyyyyyyyyyyyyyyyyyyyyyyy')
 
+<<<<<<< HEAD
 # print("xxx， new linePPPPPPPP")
+=======
+print("xxx， new linePPPPPPPP")
+>>>>>>> c0a1589 (commit3)
 
 # print("xxx， new line2")
 
 print("xxx， new line3")
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> c0a1589 (commit3)
 
 print('ddddddddddddddddddddddddd')
 print('ddddddddddddddddddddddddd')
